b6ecef1ecb7856d5397b5ffe9e4fdde22e227f49
Graham Holtslander <menello@gmail.com>
2015-03-17 22:47:31 -0600

    Updating to 1-3-2
    Adding dates to the PO details page
    Ensuring a limit of 500 characters on multiline product descriptions

---
+++ src/static/js/limitCharacters.js
@@ -0,0 +1,20 @@
+(function ($) {
+  'use strict';
+
+  $('#multiline-product').on('keydown blur', function (e) {
+    var $textArea = $(e.currentTarget);
+    var $warningDiv = $('#text-too-long');
+    var $createFormSubmitBtn = $('#create-form-submit');
+
+    if ($textArea.val().length >= 500) {
+      $('#characters').html($textArea.val().length);
+      $warningDiv.removeClass('hidden');
+      $createFormSubmitBtn.prop('disabled', true);
+    } else {
+      if (!$warningDiv.hasClass('hidden')) {
+        $warningDiv.addClass('hidden');
+      }
+      $createFormSubmitBtn.prop('disabled', false);
+    }
+  });
+} (this.$));
