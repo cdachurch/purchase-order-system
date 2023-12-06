9cc241b2356187eeafb1e0635cb74e27722451b2
Graham <menello@gmail.com>
2023-12-06 09:04:37 -0600

    add cbayles as approval admin, remove agagne

--- src/settings.py
+++ src/settings.py
@@ -7,40 +7,42 @@ STATIC_VERSION_NUMBER = 1
 
 ENVIRONMENT = "PROD"
 SERVER_ADDRESS = "https://po.cdac.ca/"
-if os.environ['APPLICATION_ID'].find("cdac-demo-purchaseorder") >= 0:
+if os.environ["APPLICATION_ID"].find("cdac-demo-purchaseorder") >= 0:
     SERVER_ADDRESS = "https://cdac-demo-purchaseorder.appspot.com/"
     ENVIRONMENT = "DEMO"
-if 'SERVER_SOFTWARE' in os.environ and os.environ['SERVER_SOFTWARE'].startswith('Development'):
+if "SERVER_SOFTWARE" in os.environ and os.environ["SERVER_SOFTWARE"].startswith(
+    "Development"
+):
     SERVER_ADDRESS = "http://localhost:8080/"
     ENVIRONMENT = "LOCAL"
 
 APPROVAL_ADMINS = [
     # "gholtslander",
     # "gdholtslander",
-    "agagne",
+    "cbayles",
     "smyhre",
     # Only for local development
-    "test@example.com"
+    "test@example.com",
 ]
 
 FINANCE_ADMINS = [
     "dwiebe",
     # Only for local development
-    "test@example.com"
+    "test@example.com",
 ]
 
 CAN_SEE_ALL_POS = APPROVAL_ADMINS + FINANCE_ADMINS
 
 
 def is_approval_admin(email):
-    """ Email addresses that can approve purchase orders """
+    """Email addresses that can approve purchase orders"""
     return email in APPROVAL_ADMINS
 
 
 def is_finance_admin(email):
-    """ Email addresses that can do finance related tasks to POs (cancel them, mainly) """
+    """Email addresses that can do finance related tasks to POs (cancel them, mainly)"""
     return email in FINANCE_ADMINS
 
 
-POS_FOR_PURCHASER_MEMCACHE_KEY = 'all-pos-for-{}'
-ALL_POS_ORDERED_MEMCACHE_KEY = 'all-pos-for-order-{}'
+POS_FOR_PURCHASER_MEMCACHE_KEY = "all-pos-for-{}"
+ALL_POS_ORDERED_MEMCACHE_KEY = "all-pos-for-order-{}"
