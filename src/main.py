2f234e668c4c73bfb17687c1ecb6cce1715d4a6b
Graham <menello@gmail.com>
2024-01-03 07:58:09 -0600

    tidy: more cleanup

--- src/main.py
+++ src/main.py
@@ -27,30 +27,3 @@ app.register_blueprint(auth.bp)
 app.register_blueprint(purchase.bp)
 app.register_blueprint(user.bp)
 app.register_blueprint(purchases_api.bp)
-
-# ROUTES = [
-#     Route(r'/api/v1/purchase/create/', handler='app.views.api.v1.purchases.PurchaseCreateApi'),
-#     Route(r'/api/v1/purchase/create/interim/', handler='app.views.api.v1.purchases.CreateInterimPurchaseApi'),
-#     Route(r'/api/v1/purchase/accept/<po_id:.{12}>/', handler='app.views.api.v1.purchases.PurchaseAcceptApi'),
-#     Route(r'/api/v1/purchase/cancel/<po_id:.{12}>/', handler='app.views.api.v1.purchases.PurchaseCancelApi'),
-#     Route(r'/api/v1/purchase/deny/<po_id:.{12}>/', handler='app.views.api.v1.purchases.PurchaseDenyApi'),
-#     Route(r'/api/v1/purchases/get/', handler='app.views.api.v1.purchases.PurchaseGetApi'),
-#     Route(r'/api/v1/purchase/invoice/<po_id:.{12}>/', handler='app.views.api.v1.purchases.PurchaseInvoiceApi'),
-
-#     RedirectRoute(r'/superadmin/', handler='app.views.superadmin.IndexView', strict_slash=True,
-#                   name='superadmin_home'),
-
-#     RedirectRoute(r'/purchase/', handler='app.views.purchase.PurchaseListView',
-#                   strict_slash=True, name='list_purchases'),
-#     RedirectRoute(r'/purchase/all/', handler='app.views.purchase.AllPurchaseListView',
-#                   strict_slash=True, name='list_purchases'),
-#     RedirectRoute(r'/purchase/create/', handler='app.views.purchase.PurchaseCreateView',
-#                   strict_slash=True, name='create_purchase'),
-#     RedirectRoute(r'/purchase/<po_id:.{12}>/', handler='app.views.purchase.PurchaseView',
-#                   strict_slash=True, name='view_purchase'),
-
-#     RedirectRoute('/user/new/', handler='app.views.user.NewUserView',
-#                   strict_slash=True, name='new_user'),
-
-#     Route(r'/', handler='app.views.main.MainView', name='index'),
-# ]
